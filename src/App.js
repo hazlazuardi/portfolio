02fdd48c7d9b397c083020ef4eae54ec30242749
hazlazuardi <56216332+hazlazuardi@users.noreply.github.com>
2021-02-05 01:39:12 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import './App.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import React, { Fragment, Suspense } from 'react';
-import { CssBaseline, ThemeProvider, withWidth } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { HazTheme } from './theme';
-import { urls } from './urls';
+import "./App.css";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import React, { Fragment, Suspense } from "react";
+import { CssBaseline, ThemeProvider, withWidth } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { HazTheme } from "./theme";
+import { urls } from "./urls";
 
-import Drawer from './components/Drawer';
-import TabBar from './components/TabBar';
-import logo from './logo.svg';
+import Drawer from "./components/Drawer";
+import TabBar from "./components/TabBar";
+import logo from "./logo.svg";
 // PWA IOS
 // import useIsIOS from './useIsIOS';
-import PWAPrompt from 'react-ios-pwa-prompt';
+import PWAPrompt from "react-ios-pwa-prompt";
 
 const useStyles = makeStyles((theme) => ({
-	content: {
-		position: 'absolute',
-		top: '0em',
-		[theme.breakpoints.up('sm')]: {
-			position: 'absolute',
-			top: '4em'
-		},
-		width: '100%'
-	}
+  content: {
+    position: "absolute",
+    top: "0em",
+    [theme.breakpoints.up("sm")]: {
+      position: "absolute",
+      top: "4em"
+    },
+    width: "100%"
+  }
 }));
 
 const theme = HazTheme;
@@ -30,44 +30,46 @@ const theme = HazTheme;
 const pages = urls;
 
 const FallBackSuspense = () => {
-	return <h1>Loading..</h1>;
+  return <h1>Loading..</h1>;
 };
 function App(props) {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	// PWA IOS
-	// const { prompt } = useIsIOS();
-	return (
-		<Fragment>
-			<CssBaseline />
-			<ThemeProvider theme={theme}>
-				{/* {prompt && alert('Install PWA')} */}
-				<PWAPrompt
-					promptOnVisit={1}
-					timesToShow={5}
-					copyClosePrompt="Close"
-					permanentlyHideOnDismiss={false}
-					debug={false}
-				/>
-				<Router>
-					<Suspense fallback={<FallBackSuspense />}>
-						{
-							props.width === 'xs' ? <TabBar logo={logo} /> :
-							<Drawer logo={logo} />}
-						{/* <Hidden smUp />
+  // PWA IOS
+  // const { prompt } = useIsIOS();
+  return (
+    <Fragment>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        {/* {prompt && alert('Install PWA')} */}
+        <PWAPrompt
+          promptOnVisit={1}
+          timesToShow={5}
+          copyClosePrompt="Close"
+          permanentlyHideOnDismiss={false}
+          debug={false}
+        />
+        <Router>
+          <Suspense fallback={<FallBackSuspense />}>
+            {props.width === "xs" ? (
+              <TabBar logo={logo} />
+            ) : (
+              <Drawer logo={logo} />
+            )}
+            {/* <Hidden smUp />
 						<Hidden xsDown>
 							<Drawer />
 						</Hidden> */}
-						<div className={classes.content}>
-							{pages.map(({ path, component }, key) => (
-								<Route exact path={path} component={component} key={key} />
-							))}
-						</div>
-					</Suspense>
-				</Router>
-			</ThemeProvider>
-		</Fragment>
-	);
+            <div className={classes.content}>
+              {pages.map(({ path, component }, key) => (
+                <Route exact path={path} component={component} key={key} />
+              ))}
+            </div>
+          </Suspense>
+        </Router>
+      </ThemeProvider>
+    </Fragment>
+  );
 }
 
 export default withWidth()(App);
