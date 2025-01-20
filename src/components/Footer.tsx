79b2f134dfb46cf8aedef562a5b9ef4d31d716fe
hazlazuardi <hazlazuardi@gmail.com>
2025-01-21 03:44:19 +1000

    fix: apos escape

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer className='w-screen h-[66vh] bg-black text-2xl sm:text-4xl sm:leading-relaxed place-content-center px-6 md:px-24'>
             <p className='max-w-4xl mx-auto'>
-                Open to new opportunities or side hustles. Let&aposs build cool stuffs! <EmailLink email='hazlazuardi@gmail.com'>hazlazuardi@gmail.com</EmailLink>
+                Open to new opportunities or side hustles. Let&apos;s build cool stuffs! <EmailLink email='hazlazuardi@gmail.com'>hazlazuardi@gmail.com</EmailLink>
             </p>
         </footer>
     )
